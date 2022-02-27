d3d80b28077e9d12b5f2dbbd8318ef9d152d29a3
wam <dorothy_007@naver.com>
2022-02-28 03:14:11 +0900

    2022 공간

--- src/page/main/WSLParticipationCountries.tsx
+++ src/page/main/WSLParticipationCountries.tsx
@@ -12,7 +12,7 @@ const WSLParticipationCountries = (): React.ReactElement | null => {
     <Wrapper mobileWidth={mobileWidth}>
       <BgStyle bgUrl="bg_world_map" position="bottom center" />
       <TitleBox isMobile={isMobile}>
-        <h2>WSL 2022</h2>
+        <h2>WSL 2022-</h2>
         <h2>PARTICIPATING COUNTRIES</h2>
       </TitleBox>
       <ParticipationBox isMobile={isMobile}>
@@ -27,6 +27,7 @@ const Wrapper = styled.div<{ mobileWidth: string }>`
 `;
 
 const TitleBox = styled.div<{ isMobile: boolean }>`
+  margin-top: ${({ isMobile }) => isMobile && '0.5rem'};
   h2 {
     font-size: ${({ isMobile }) => isMobile && '4rem'};
   }
